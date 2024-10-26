3c213209d2751d0c6740e0310165800c58fec9cb
johnykosmos <kumakpospolity@gmail.com>
2024-10-26 12:30:59 +0200

    Feat: Final resolve for pages

--- src/dialogueLogic.js
+++ src/dialogueLogic.js
@@ -25,7 +25,7 @@ export function setDialogueIndex(index){
 }
 
 export async function setCurrentDialogue(filename){
-    const response = await fetch(`kotitest/dialogues/${filename}`);
+    const response = await fetch(`kotitest/dialogues/${filename}.json`);
     const data = await response.json();
     currentDialogue = data;
     dialogueIndex = 0;
